e0f4e81a2f59ccd0dc44d9ed73dc8401b6ffc410
Jaz Baliola <baliola05@gmail.com>
2023-08-01 13:41:34 -0600

    Update user.js

--- user.js
+++ user.js
@@ -147,7 +147,7 @@ router.get('/new', (req, res) => {
         .then(([rows, fields]) => {
             // check if query affected a row
             if(rows.affectedRows > 0) {
-                res.redirect(callback+"#userAddedTrue");
+                navigate("/adminPanel#userAddedTrue");
             } else {
                 res.redirect(callback+"#userAddedFalse");
             }
